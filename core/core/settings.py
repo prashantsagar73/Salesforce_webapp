5f69b3f83007d5dfd8087938c7b71fcb121a59bc
prashantsagar73 <prashant.sagar73@gmail.com>
2021-07-12 13:11:03 +0530

    model & serializers created

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'sales',
+    'rest_framework',
+    # 'salesforce',
 ]
 
 MIDDLEWARE = [
--- core/sales/models.py
+++ core/sales/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+TITLE_CHOICES = (
+    ('Mr', 'Mr'),
+    ('Mrs.', 'Mrs'),
+    ('Ms', 'Ms'),
+)
+
+class Contact(models.Model):
+    Title = models.CharField(choices=TITLE_CHOICES, max_length=19, blank=False, null=False)
+    FirstName = models.CharField(max_length=255, blank=True, null=True)
+    LastName = models.CharField(max_length=255, blank=False, null=False)
+    Email = models.CharField(max_length=255, blank=False, null=False, unique=True)
+    Phone = models.CharField(max_length=255, blank=False, null=False, unique=True)
+
+    def __str__(self):
+        return self.FirstName
