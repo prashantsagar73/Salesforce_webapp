faa54a7fa4c595be8ef2c92a7104c3febf5bf5cb
prashantsagar73 <prashant.sagar73@gmail.com>
2021-07-13 17:21:02 +0530

    remove unwanted files form sales

--- core/sales/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Contact
-
-# serializers class for model class 
-class ContactSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Contact
-        fields = '__all__'
--- core/salesforeauth/utils.py
+++ core/salesforeauth/utils.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 
-from django_salesforce_oauth.oauth import OAuth
+from salesforeauth.oauth import OAuth
 
 
 def get_or_create_user(oauth: OAuth):
